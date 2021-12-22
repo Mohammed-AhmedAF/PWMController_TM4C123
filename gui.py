b54ae690f10cc6da4b85ea7acf25b566b3631c42
Mohammed Ahmed <mohamedstarwars@gmail.com>
2021-12-22 22:44:54 +0200

    Updated GUI to work with two PWMs

--- gui.py
+++ gui.py
@@ -2,14 +2,15 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 import platform
+from typing import List
 import serial
 import time
+from functools import partial
 
-def sendPWM1():
-    pwmVarInt = int(pwm1Var.get())
-    print(pwmVarInt)
-    ser.write(b'a')
-    time.sleep(0.20)
+def sendPWM(pwmID):
+    pwmVarInt = int(pwmList[pwmID].get())
+    ser.write(pwmID.to_bytes(1,'little'))
+    time.sleep(0.1)
     ser.write(pwmVarInt.to_bytes(1,'little'))
 
 def connectByUART() -> None:
@@ -33,6 +34,10 @@ def connectByUART() -> None:
         else:
             statusLabel['text'] = "Connection already opened!"
 
+def disconnectUART():
+    global ser
+    ser.close()
+
 #Global variables
 ser = serial.Serial()
 baudrate = 0
@@ -53,19 +58,22 @@ baudrateCmbox.current(0)
 portCmbox = ttk.Combobox(connectionFrame,values=["COM3","COM4","COM5","COM6","COM7","COM8","COM9","COM10","COM11","COM12","COM13"],state="readonly")
 portCmbox.current(1)
 connectBtn = Button(connectionFrame,text="Connect",command=connectByUART)
+disconnectBtn = Button(connectionFrame,text="Disconnect",command=disconnectUART)
 
 PWM1Frame = LabelFrame(controlFrame,text="PWM1")
-PWM1SendButton = Button(PWM1Frame,text="Send",command=sendPWM1)
+PWM1SendButton = Button(PWM1Frame,text="Send",command=partial(sendPWM,0))
 PWM2Frame = LabelFrame(controlFrame,text="PWM2")
-PWM2SendButton = Button(PWM2Frame,text="Send")
+PWM2SendButton = Button(PWM2Frame,text="Send",command=partial(sendPWM,1))
 pwm1Var = DoubleVar()
-PWM1Scale = Scale(PWM1Frame,variable=pwm1Var,from_=1,to=100,orient=HORIZONTAL)
+pwmList = [DoubleVar(), DoubleVar()]
+PWM1Scale = Scale(PWM1Frame,variable=pwmList[0],from_=1,to=100,orient=HORIZONTAL)
 pwm2Var = DoubleVar()
-PWM2Scale = Scale(PWM2Frame,variable=pwm2Var,from_=1,to=100,orient=HORIZONTAL)
+PWM2Scale = Scale(PWM2Frame,variable=pwmList[1],from_=1,to=100,orient=HORIZONTAL)
 connectionFrame.grid(row=0,column=3,sticky=W+S+N+E,padx=5)
 controlFrame.grid(row=0,column=0,sticky=W+S+N+E,padx=5,columnspan=3)
 
 connectBtn.grid(row=2,column=0,sticky=W+E,padx=5,pady=5)
+disconnectBtn.grid(row=3,column=0,stick=W+E,padx=5,pady=5)
 baudrateCmbox.grid(row=1,column=0,sticky=W+E,padx=5,pady=5)
 portCmbox.grid(row=0,column=0,sticky=W+E,padx=5,pady=5)
 
